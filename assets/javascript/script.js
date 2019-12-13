c5a603d9e65df088803dd8fe4a5f5152391ed126
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2019-12-12 21:04:32 -0500

    clear button is working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   var numberRecordsEl = $("#number-records");
   var startYearEl = $("#start-year");
   var endYearEl = $("#end-year");
+  var articlesGoHereEl = $("#articlesGoHere");
 
   // Create click event for Search button
   var searchBtn = $("#searchBtn");
@@ -18,10 +19,24 @@ $(document).ready(function() {
     console.log(numberRecordsVal);
     console.log(startYear);
     console.log(endYear);
+    // Need for loop here
+    // Create a new div within articlesGoHereEl to house each article returned from API
+    var newDiv = $("<div>");
+    // Do things to the div
+    articlesGoHereEl.append(newDiv);
   });
+
   // Create click event for Clear button
   var clearBtn = $("clearBtn");
   clearBtn.on("click", function() {
     event.preventDefault();
+    // Empty all text inputs
+    searchTermEl.empty();
+    startYearEl.empty();
+    endYearEl.empty();
+    // Reset number of records to 5
+    numberRecordsEl.val(5);
+    // Clear the articles div
+    articlesGoHereEl.empty();
   });
 });
