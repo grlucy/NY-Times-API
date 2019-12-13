e18141c3e9fcf5dfce1568e335d4a7393ea23c39
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2019-12-12 21:29:08 -0500

    added back end code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,15 +15,42 @@ $(document).ready(function() {
     var numberRecordsVal = numberRecordsEl.val();
     var startYear = startYearEl.val();
     var endYear = endYearEl.val();
-    console.log(searchTermVal);
-    console.log(numberRecordsVal);
-    console.log(startYear);
-    console.log(endYear);
-    // Need for loop here
-    // Create a new div within articlesGoHereEl to house each article returned from API
-    var newDiv = $("<div>");
-    // Do things to the div
-    articlesGoHereEl.append(newDiv);
+    // Interface with API
+    // queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=canada&api-key=PVhkpNf6oRHLZzBRz8qh2olTphYL39Rx";
+    queryURL =
+      "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+      searchTermVal +
+      "&fq=pub_year:[" +
+      startYear +
+      "%20TO%20" +
+      endYear +
+      "]&api-key=PVhkpNf6oRHLZzBRz8qh2olTphYL39Rx";
+    console.log(queryURL);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(result) {
+      console.log(result);
+      //log title
+      console.log(result.response.docs[7].headline.main);
+      //log author
+      console.log(result.response.docs[7].byline.original);
+      //log section
+      console.log(result.response.docs[7].section_name);
+      //log publication date
+      console.log(result.response.docs[7].pub_date);
+      //log URL
+      console.log(result.response.docs[7].web_url);
+      for (var i = 0; i < numberRecordsVal; i++) {
+        // Create a new div within articlesGoHereEl to house each article returned from API
+        var newDiv = $("<div>");
+        // Adding title to div
+        newDiv.append(
+          $("<h1>" + result.response.docs[i].headline.main + "</h1>")
+        );
+        articlesGoHereEl.append(newDiv);
+      }
+    });
   });
 
   // Create click event for Clear button
